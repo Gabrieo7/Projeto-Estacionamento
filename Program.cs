f3133ffed0ae8cb2e05e0390c96f1f6ea568abf0
Gabrieo7 <gab.azevedo97@gmail.com>
2024-09-05 22:25:10 -0300

    estacionamento

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using DesafioFundamentos.Models;
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+Console.WriteLine("Seja bem-vindo ao sistema de estacionamento!\n");
+Console.WriteLine("Digite o preço inicial:");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+// Realiza o loop do menu
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+
+        case "2":
+            es.RemoverVeiculo();
+            break;
+
+        case "3":
+            es.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            Console.WriteLine("Encerrando o programa...");
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida! Tente novamente.");
+            break;
+    }
+
+    // Se a opção não for "Encerrar", esperar uma tecla para continuar
+    if (exibirMenu)
+    {
+        Console.WriteLine("Pressione uma tecla para continuar");
+        Console.ReadLine();
+    }
+}
+
+Console.WriteLine("O programa se encerrou.");
+
